8c1c31871fcd41d547985d7c6477184a80d2defb
younggil94 <younggil9488@gmail.com>
2020-03-20 18:45:02 +0900

    예외처리 + 클래스처리

--- JavaScript/time.js
+++ JavaScript/time.js
@@ -1,35 +1,12 @@
-/*
-1. 입력형태
-data = {
-  start: 2020-03-17T04:30:00.000Z(Date형식)
-  end: 2020-03-17T05:30:00.000Z(Date형식)
-  color: "green"
-};
+module.exports = class TimeTable {
+  constructor() {
+    this.studytime = [];
+    for (let i = 0; i < 24; ++i) {
+      this.studytime.push([]);
+    }
+  }
 
-2. 출력형태
-[
-  <11 empty items>,
-  [ { start: 0, end: 59, color: '#123456' } ],
-  [ { start: 0, end: 59, color: '#123456' } ],
-  [ { start: 0, end: 10, color: '#123456' } ],
-  <10 empty items>
-]
-*/
-
-const _TIME = require("./_TIME.js");
-
-exports.difftime = data => {
-  const resulttime = [];
-  for (let i = 0; i < data.length; i++) {
-    let time = new _TIME.TimeClass(data[i].start, data[i].end, data[i].color);
-    // console.log(time);
-
-    // _TIME.TimeFunc.prototype.getTime = function() {
-    //   return new _TIME.TimeFunc(data[i].start, data[i].end, data[i].color);
-    // };
-    // console.log(_TIME.TimeFunc.prototype.getTime());
-
-    const studytime = new Array(24);
+  pushTime(time) {
     // 이후시간보다 이전시간이 클경우 예외처리
     if (time.end - time.start < 0) {
       const tmp = time.start;
@@ -39,14 +16,15 @@ exports.difftime = data => {
 
     const timeGap = new Date(0, 0, 0, 0, 0, 0, time.end - time.start);
     const diffHour = timeGap.getHours();
-    const diffMin = timeGap.getMinutes();
+    const diffMin = time.start.getMinutes() + timeGap.getMinutes();
+
     // 차이가 1시간 이내
-    if (diffHour == 0 && time.start.getMinutes() + diffMin <= 60) {
-      studytime[time.start.getHours()] = new _TIME.TimeClass(
-        time.start.getMinutes(),
-        time.start.getMinutes() + diffMin,
-        time.color
-      );
+    if (diffHour == 0 && diffMin <= 60) {
+      this.studytime[time.start.getHours()].push({
+        start: time.start.getMinutes(),
+        end: diffMin,
+        color: time.color
+      });
     }
 
     // 차이가 1시간 이상
@@ -58,45 +36,91 @@ exports.difftime = data => {
         time.start.getFullYear() !== time.end.getFullYear()
       ) {
         // 시작값은 초기 분값 ~ 59까지
-        studytime[time.start.getHours()] = new _TIME.TimeClass(
-          time.start.getMinutes(),
-          59,
-          time.color
-        );
+        this.studytime[time.start.getHours()].push({
+          start: time.start.getMinutes(),
+          end: 59,
+          color: time.color
+        });
 
         // 나머지 전부색칠
         for (let i = time.start.getHours() + 1; i <= 24; i++) {
-          studytime[i] = new new _TIME.TimeClas(0, 59, time.color)();
+          this.studytime[i].push({
+            start: 0,
+            end: 59,
+            color: time.color
+          });
         }
       }
 
       // 다음날까지 선택하지 않은 경우
       else {
         // 시작값은 초기 분값 ~ 59까지
-        studytime[time.start.getHours()] = new _TIME.TimeClass(
-          time.start.getMinutes(),
-          59,
-          time.color
-        );
+        this.studytime[time.start.getHours()].push({
+          start: time.start.getMinutes(),
+          end: 59,
+          color: time.color
+        });
 
         // 그 사이 시간은 전부 색칠
         for (let i = 1; i <= diffHour; i++) {
-          studytime[time.start.getHours() + i] = new _TIME.TimeClass(
-            0,
-            59,
-            time.color
-          );
+          this.studytime[time.start.getHours() + i].push({
+            start: 0,
+            end: 59,
+            color: time.color
+          });
         }
 
         // 끝값은 0 ~ 초기 분값
-        studytime[time.end.getHours()] = new _TIME.TimeClass(
-          0,
-          time.end.getMinutes(),
-          time.color
-        );
+        this.studytime[time.end.getHours()].push({
+          start: 0,
+          end: time.end.getMinutes(),
+          color: time.color
+        });
       }
     }
-    resulttime.push(studytime);
+    return this.studytime;
   }
-  return resulttime;
+
+  checkInTime(time, index) {
+    let StartHour = time.start.getHours();
+    let EndHour = time.end.getHours();
+    let StartMin = time.start.getMinutes();
+    let EndMin = time.end.getMinutes();
+
+    let ErrorTime = [];
+
+    if(this.studytime[StartHour].length > 0 && this.studytime[StartHour][0].start <= StartMin){
+      ErrorTime.push(index + "번째 데이터[0]번째 줄 : " + JSON.stringify(time));
+    }
+    
+    if(this.studytime[EndHour].length > 0 && this.studytime[EndHour][0].end >= EndMin){
+      ErrorTime.push(index + "번째 데이터[" + this.studytime[EndHour].length + "]번째 줄 : " + JSON.stringify(time))
+    }
+
+    for(let i=StartHour+1; i< EndHour; i++){
+      if(this.studytime[i].length > 0){
+        ErrorTime.push(index + "번째 데이터[" + i + "]번째 줄 : " + JSON.stringify(time));
+      }
+    }
+
+    return {
+      status : ErrorTime.length > 0 ? false : true,
+      msg : ErrorTime.length > 0 ? ErrorTime : index + "번째 데이터 등록성공"
+    }
+  }
+
+  difftime(TimeData) {
+    
+    // 예외처리
+    TimeData.forEach((time, index) => console.log(this.checkInTime(time, index)));
+
+    // 객체일 경우
+    if (!Array.isArray(TimeData)) {
+      return this.pushTime(TimeData);
+    }
+
+    // 배열일 경우
+    TimeData.forEach(time => this.pushTime(time));
+    return this.studytime;
+  }
 };
