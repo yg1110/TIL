3a0cab15b448479d1868ae88e5ee1ab81a831c47
yg1110 <younggil9488@gmail.com>
2020-04-15 23:28:28 +0900

    알고리즘 풀이

---
+++ JavaScript/algo/FINN.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  let answer = 1;
+  for (let i = 1; i < n; i++) {
+    let sum = i;
+    for (let j = i + 1; j <= n; j++) {
+      sum += j;
+      if (sum === n) {
+        answer++;
+        break;
+      }
+      if (sum > n) break;
+    }
+  }
+  return answer;
+}
+console.log(solution(15));
