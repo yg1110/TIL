9f24ec14b9a9375fcbfc101e3c14aa2d11829abe
yg1110 <younggil9488@gmail.com>
2020-07-07 01:35:58 +0900

    todo crud까지

--- React/todo/src/App.js
+++ React/todo/src/App.js
@@ -1,32 +1,60 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const state = {
-    todoList: ["abc", "22"]
-  };
-  function addTodoList(e) {
+  const [todoList, setTodoList] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+  const inputRef = useRef(null);
+
+  const addTodoList = () => {
     const input = document.querySelector(".todoInput");
 
-    // console.log(state.todoList);
-    // state.todoList.push(input.value);
-    this.state.setState(prevState => {
-      return {
-        todoList: [...prevState.todoList, prevState.todoList.push(input.value)]
-      };
+    setTodoList(prevState => {
+      return [...prevState, input.value];
     });
-    console.log(input.value);
-    console.log(state.todoList);
-  }
+    setTodoInput("");
+    inputRef.current.focus();
+  };
+
+  const onChange = e => {
+    setTodoInput(e.target.value);
+  };
+
+  const deleteTodoList = i => {
+    let deletetodo = [...todoList];
+    deletetodo.splice(i, 1);
+    setTodoList(deletetodo);
+  };
+
+  const updateTodoList = i => {
+    const todoListValue = document.querySelector(".todoList" + i);
+    const updateTodo = prompt("수정할 값", todoListValue.innerHTML);
+    let newTodoList = [...todoList];
+    newTodoList.splice(i, 1, updateTodo);
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
       <div className="inputForm">
-        <input className="todoInput" type="text"></input>
+        <input
+          className="todoInput"
+          type="text"
+          value={todoInput}
+          onChange={onChange}
+          ref={inputRef}
+        ></input>
         <button onClick={addTodoList}>추가</button>
       </div>
-      {state.todoList.map((v, i) => (
-        <div key={v + i}>{v}</div>
-      ))}
+      {todoList.map((v, i) => {
+        return (
+          <div key={v + i}>
+            <div className={"todoList" + i}>{v}</div>
+            <button onClick={() => updateTodoList(i)}>수정</button>
+            <button onClick={() => deleteTodoList(i)}>삭제</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
